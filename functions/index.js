f91918d936c09fb5b80d4b4be7d593270ad1562e
Isman Usoh <isman.usoh@gmail.com>
2019-08-04 21:13:29 +0700

    Initial  project

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+// ./functions/index.js
+const functions = require('firebase-functions');
+const next = require('next')
+const app = next({ dev: false, conf: { distDir: "next" } })
+const handle = app.getRequestHandler()
+
+exports.nextApp = functions.https.onRequest((request, response) => {
+    console.log("[Function] RequestUrl", request.originalUrl)
+    return app.prepare().then(() => handle(request, response))
+})
